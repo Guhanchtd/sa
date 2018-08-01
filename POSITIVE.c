0b8a8e360a7a98b58a61674e3b8d360816afca7a
Guhanchtd <35605321+Guhanchtd@users.noreply.github.com>
2018-08-01 09:48:54 +0530

    Update POSITIVE.c

--- POSITIVE.c
+++ POSITIVE.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int num;
+  scanf("%d",&num);
+  if(num>0)
+    printf("positive")';
+ if(num==0)
+   printf("zero");
+  else if(num<0)
+    printf("negative");
+}
