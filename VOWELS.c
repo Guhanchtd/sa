8dda3490e8a592d16af399178981d55679fa58e3
Guhanchtd <35605321+Guhanchtd@users.noreply.github.com>
2018-08-01 10:09:22 +0530

    Update VOWELS.c

--- VOWELS.c
+++ VOWELS.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  char ch;
+  scanf("%c",&ch);
+  if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    printf("Vowel");
+  else
+    printf("Consonant");
+  if(ch=='!'||ch=='@'||ch=='#'||ch=='$'||ch=='%'||ch=='^'||ch=='&'||ch=='*'||ch=='('||ch==')'||ch=='/'||ch=='\')
+     printf("invalid");
