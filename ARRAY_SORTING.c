fee8da26a65b2a44d7e84d5db55170621736f233
Guhanchtd <35605321+Guhanchtd@users.noreply.github.com>
2018-08-20 16:16:36 +0530

    Create ARRAY_SORTING.c

---
+++ ARRAY_SORTING.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,arr[num],temp=0;
+scanf("%d",&num);
+for(int i=0;i<num;i++)
+{
+scanf("%d",&arr[i]);
+}
+for(int i=0;i,numi++)
+{
+for(int j=0;j<num;j++)
+{
+if(arr[i]>arr[j])
+{
+temp=a[i];
+a[i]=a[j];
+temp=a[j];
+}
+}
+}
+for(int i=0;i<num;i++)
+{
+printf("%d",a[i]);
+}
+}
